fd0fd29072433fcfc3309dcd2d763ee426d73816
yuriy <yurii.shvets@sharafdg.com>
2020-06-08 13:36:57 +0300

    Fix port

--- server.js
+++ server.js
@@ -1,21 +1,24 @@
-const express = require('express');
-const path = require('path');
-const serveStatic = require('serve-static');
-const { createProxyMiddleware } = require('http-proxy-middleware');
-const dotenv = require('dotenv');
+const express = require("express");
+const path = require("path");
+const serveStatic = require("serve-static");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+const dotenv = require("dotenv");
 
 const dotenvResult = dotenv.config();
 const env = dotenvResult.parsed;
 
 const app = express();
 app.use(serveStatic(__dirname + "/dist"));
-app.use('/api', createProxyMiddleware({
-  target: env.API_BASE_URL,
-  changeOrigin: true,
-  pathRewrite: function(path) {
-    return path.replace("/api", env.API_BASE_URL) + `&apiKey=${env.API_KEY}`;
-  }
-}));
-const port = env.PORT || 5000;
+app.use(
+  "/api",
+  createProxyMiddleware({
+    target: env.API_BASE_URL,
+    changeOrigin: true,
+    pathRewrite: function(path) {
+      return path.replace("/api", env.API_BASE_URL) + `&apiKey=${env.API_KEY}`;
+    }
+  })
+);
+const port = process.env.PORT || 5000;
 app.listen(port);
-console.log('server started '+ port);
+console.log("server started " + port);
